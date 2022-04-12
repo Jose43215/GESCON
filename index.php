62cc3301cfef23a08a9157b76c8489e5d14b2516
Jose43215 <pepe-alberto@live.com.mx>
2022-04-11 23:37:37 -0500

    Continuación de estructuración de la pagina

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <header class="parallax">
+    <div class="parallax-text">
+      <h2>CONCILIORG</h2>
+      <P>Ofreciendo soluciones integrales para congresos</P>
+    </div>
+    <div class="abajo">
+      <div class="desliza">
+        <p>Desliza hacia abajo</p>
+        <img src="Recursos/Iconos/bx-chevrons-down.svg" alt="">
+      </div>
+    </div>
+  </header>
+  <article class="nosotros">
+    <p.tituloPag>NOSOTROS</p>
+  </article>
+</body>
+</html>
