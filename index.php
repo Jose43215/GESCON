d348ac2f4805385da9489e8cd58a64708b399600
Jose43215 <pepe-alberto@live.com.mx>
2022-04-13 10:46:05 -0500

    Index

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  <link rel="stylesheet" href="Css/indexCSS.css">
 </head>
 <body>
   <header class="parallax">
@@ -20,7 +21,57 @@
     </div>
   </header>
   <article class="nosotros">
-    <p.tituloPag>NOSOTROS</p>
+    <p class="tituloPag">NOSOTROS</p>
+      <section class="card">
+        <img src="Recursos/Imagenes/cia-80721_1920.jpg" alt="">
+        <p class="nombrePaquete">Nombre</p>
+        <p class="textDescripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <a href="#" class="botonVer">Ver</a>
+      </section>
+      <section class="card">
+        <img src="Recursos/Imagenes/cia-80721_1920.jpg" alt="">
+        <p class="nombrePaquete">Nombre</p>
+        <p class="textDescripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <a href="#" class="botonVer">Ver</a>
+      </section>
+      <section class="card">
+        <img src="Recursos/Imagenes/cia-80721_1920.jpg" alt="">
+        <p class="nombrePaquete">Nombre</p>
+        <p class="textDescripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <a href="#" class="botonVer">Ver</a>
+      </section>
   </article>
+  <aside class="banner">
+    <div class="info">
+      <h2>
+        TEXTO DE ENTRADA AQUÍ<br>
+        texto 2
+      </h2>
+      <a href="#" class="botonVerBanner"></a>
+    </div>
+  </aside>
+  <article class="verano">
+    <p class="tituloPag">TEXTO METIDO 2</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </article>
+  <article class="banner3"></article>
+  <article class="verano">
+    <p class="tituloPag">TEXTO RUEBA 3</p>
+    <p>  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </article>
+  <footer>
+    <div class="pie">
+      <p>&copy; COPYRIGHT | DERECHOS RESERVADOS | CONCILIORG</p>
+    </div>
+  </footer>
 </body>
 </html>
